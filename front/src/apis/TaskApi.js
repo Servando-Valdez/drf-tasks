e1f09bfadc950829ffb41135bfe28e343b7769f2
Servando Valdez <96026888+Servando-Valdez@users.noreply.github.com>
2023-10-25 11:46:00 -0700

    api service

---
+++ front/src/apis/TaskApi.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const instance = axios.create({
+        baseURL: 'http://localhost:8000/api/v1',
+})
+
+export class TaskService{
+        
+        async getTasks(){
+                const response = await instance.get('/tasks');
+                return response.data;
+        }
+
+        async getTask(uuid){
+                const response = await instance.get(`/tasks/${uuid}`);
+                return response.data;
+        }
+        
+        async createTask(task){
+                const response = await instance.post('/tasks', task);
+                return response.data;
+        }
+        
+        async updateTask(task) {
+                const response = await instance.put(`/tasks/${task.uuid}`, task);
+                return response.data;
+        }
+
+        async deleteTask(task){
+                const response = await instance.delete(`/tasks/${task.uuid}`);
+                return response.data;
+        }
+}
