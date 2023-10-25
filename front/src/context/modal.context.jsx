01c1cc995485ef2b6c4ab449c98d1080d09fd405
Servando Valdez <96026888+Servando-Valdez@users.noreply.github.com>
2023-10-25 12:25:17 -0700

    modal context

---
+++ front/src/context/modal.context.jsx
@@ -0,0 +1,52 @@
+import { useState, createContext, useContext } from "react";
+import propTypes from 'prop-types';
+import { useTask } from './task.context';
+
+const ModalContext = createContext();
+
+ModalProvider.propTypes = {
+    children: propTypes.node.isRequired,
+};
+
+export function ModalProvider({ children }){
+    const { getTask } = useTask();
+    const [showDetail, setShowDetail] = useState(false);
+    const [showUpdate, setShowUpdate] = useState(false);
+
+    const handleShowUpdateClose = () => setShowUpdate(false);
+    const handleShowUpdateShow = (task) => {
+        getTask(task.uuid);
+        setShowUpdate(true);
+    }
+
+    const handleShowDetailClose = () => setShowDetail(false);
+    const handleShowDetailShow = (task) => {
+        getTask(task.uuid);
+        setShowDetail(true);
+    }
+
+    return (
+        <ModalContext.Provider
+            value={{
+                showDetail,
+                setShowDetail,
+                showUpdate,
+                setShowUpdate,
+                handleShowUpdateClose,
+                handleShowUpdateShow,
+                handleShowDetailClose,
+                handleShowDetailShow,
+            }}
+        >
+            {children}
+        </ModalContext.Provider>
+    );
+}
+
+export function useModal() {
+    const context = useContext(ModalContext);
+    if (!context) {
+        throw new Error('useTask must be used within a TaskProvider');
+    }
+    return context;
+}
