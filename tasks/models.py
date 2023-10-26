12ef9a8a933892a6d746333461d9f530fe985ac3
Servando Valdez <96026888+Servando-Valdez@users.noreply.github.com>
2023-10-26 15:49:12 -0700

    comments

--- tasks/models.py
+++ tasks/models.py
@@ -6,11 +6,37 @@ from safedelete.models import SafeDeleteModel
 import uuid
 # Create your models here.
 class AbstractModel(SafeDeleteModel):
+    """
+    Abstract class for models with safe delete functionality (Safe Delete).
+
+    Attributes:
+    - created_at: The creation date and time of the model.
+
+    This abstract class is used as a base for other models and provides safe delete functionality.
+    The 'created_at' attribute records the date and time the model was created.
+    """
+
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         abstract = True
 class Task(AbstractModel):
+    """
+    Model to represent tasks.
+
+    Attributes:
+    - uuid: Unique identifier of the task.
+    - name: Name of the task.
+    - completed: Indicates whether the task is completed or not.
+
+    Methods:
+    - __str__: Returns the task name as a string representation.
+
+    This model represents a task and inherits from 'AbstractModel'.
+    The 'uuid' attribute is used as the primary key and is an automatically generated unique identifier.
+    'name' stores the name of the task and 'completed' indicates whether the task is completed or not.
+    """
+    
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100, null=False, blank=False)
     completed = models.BooleanField(default=False)
--- tasks/serializer.py
+++ tasks/serializer.py
@@ -3,19 +3,43 @@ from .models import Task
 
 
 class TaskSeralizer(serializers.ModelSerializer):
+    """
+    Serializer for the task (Task).
 
+    Fields:
+    - uuid: The unique identifier of the task.
+    - name: The name of the task.
+    - completed: Indicates whether the task is completed or not.
+
+    This serializer is used to convert task model objects into JSON format and vice versa.
+    """
     class Meta:
         model = Task
         fields = ('uuid', 'name', 'completed')
 
 class CreateTaskSerializer(serializers.ModelSerializer):
+    """
+    Serializer for task creation.
 
+    Fields:
+    - name: The name of the task (required field).
+
+    This serializer is used to create new tasks. A JSON object with the 'name' field is expected.
+    """
     class Meta:
         model = Task
         fields = ('name',)
 
 class UpdateTaskSerializer(serializers.ModelSerializer):
+    """
+    Serializer for task update.
 
+    Fields:
+    - name: The name of the task (required field).
+    - completed: Indicates whether the task is completed or not (optional field).
+
+    This serializer is used to update existing tasks. JSON objects with 'name' and/or 'completed' fields are expected.
+    """
     class Meta:
         model = Task
         fields = ('name', 'completed')
